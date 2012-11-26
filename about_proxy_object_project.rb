aa7051a081db87b5f9459192014273890fa1792c
Johan van der Laak <johanvdlaak@hotmail.com>
2012-11-26 17:11:06 +0100

    Answer up-to 268

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -13,12 +13,24 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader :messages
+
   def initialize(target_object)
     @object = target_object
     # ADD MORE CODE HERE
+    @messages = []
   end
 
   # WRITE CODE HERE
+
+  def method_missing(method_name, *args, &block)
+    @messages.push(method_name.to_sym)
+    @object.__send__(method_name, *args, &block)
+  end
+
+  def called?
+    
+  end
 end
 
 # The proxy object should pass the following Koan:
